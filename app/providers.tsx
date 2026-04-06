98874110a0f80d3aaef9bb7c2ce168d89b8e7c26
VALT <wearevalt@gmail.com>
2026-04-06 23:40:28 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,18 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from 'next-themes'
 
 type ProvidersProps = {
   children: React.ReactNode
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  )
 }
